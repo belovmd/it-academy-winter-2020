7bc9608a689cb67591edeeee3a6d8b0abd52d7c0
dambo-dambo <denis_borisenko@mail.ru>
2020-11-15 16:10:12 +0300

    Update Homework1 Denis Borisenko.py
    Homework1: Denis Borisenko

--- it-academy-winter-2020/Homework1 Denis Borisenko.py
+++ it-academy-winter-2020/Homework1 Denis Borisenko.py
@@ -156,9 +156,13 @@ if __name__ == '__main__':
 
 # 14 lines: Doctest-based testing
 
+
 def median(pool):
-    """Statistical median to demonstrate doctest. >>> median([2, 9, 9, 7, 9, 2, 4, 5, 8])
-    6 #change to 7 in order to pass the test """
+    """
+    Statistical median to demonstrate doctest.
+    >>> median([2, 9, 9, 7, 9, 2, 4, 5, 8])
+    6 #change to 7 in order to pass the test
+    """
     copy = sorted(pool)
     size = len(copy)
     if size % 2 == 1:
@@ -350,6 +354,6 @@ while guesses_made < 6:
         break
 
 if guess == number:
-    print('Good job, {0}! You guessed my number in {1} guesses!'.format(name, guesses_made))
+    print('Good job, {0} my number in {1} guesses!'.format(name, guesses_made))
 else:
     print('Nope. The number I was thinking of was {0}'.format(number))
