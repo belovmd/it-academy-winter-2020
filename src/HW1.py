9ad062a5f1a6d8f466d8002911477d0781b84ee6
Anastasiya71 <aabarinova71@gmail.com>
2020-11-18 15:36:38 +0300

    SimplePrograms from https://wiki.python.org/moin/SimplePrograms

--- src/HW1.py
+++ src/HW1.py
@@ -1,26 +1,24 @@
 # Output
+import unittest
+from itertools import groupby
+
 print('Hello, world!')
+
 # Input, assignment
 name = input('What is your name?\n')
 print('Hi, %s.' % name)
+
 # For loop, built-in enumerate function, new style formatting
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
     print("iteration {iteration} is {name}".format(iteration=i, name=name))
+
 # Fibonacci, tuple assignment
 parents, babies = (1, 1)
 while babies < 100:
     print('This generation has {0} babies'.format(babies))
-    parents, babies = (babies, parents + babies)
-# Import, regular expressions
+    parents, babies = [babies, parents + babies]
 
-import re
-
-for test_string in ['555-1212', 'ILL-EGAL']:
-    if re.match(r'^\d{3}-\d{4}$', test_string):
-        print(test_string, 'is a valid US local phone number')
-    else:
-        print(test_string, 'rejected')
 # Dictionaries, generator expressions
 prices = {'apple': 0.40, 'banana': 0.50}
 my_purchase = {
@@ -29,23 +27,7 @@ my_purchase = {
 grocery_bill = sum(prices[fruit] * my_purchase[fruit]
                    for fruit in my_purchase)
 print('I owe the grocer $%.2f' % grocery_bill)
-# Time, conditionals, from..import, for..else
-from time import localtime
 
-activities = {8: 'Sleeping',
-              9: 'Commuting',
-              17: 'Working',
-              18: 'Commuting',
-              20: 'Eating',
-              22: 'Resting'}
-time_now = localtime()
-hour = time_now.tm_hour
-for activity_time in sorted(activities.keys()):
-    if hour < activity_time:
-        print(activities[activity_time])
-        break
-else:
-    print('Unknown, AFK or sleeping!')
 # Triple-quoted strings, while loop
 REFRAIN = '''
 %d bottles of beer on the wall,
@@ -78,46 +60,7 @@ class BankAccount(object):
 my_account = BankAccount(15)
 my_account.withdraw(50)
 print(my_account.balance, my_account.overdrawn())
-# Unit testing with unittest
-import unittest
 
-
-def median(pool):
-    copy = sorted(pool)
-    size = len(copy)
-    if size % 2 == 1:
-        return copy[int((size - 1) / 2)]
-    else:
-        return (copy[int(size / 2 - 1)] + copy[int(size / 2)]) / 2
-
-
-class TestMedian(unittest.TestCase):
-    def testMedian(self):
-        self.assertEqual(median([2, 9, 9, 7, 9, 2, 4, 5, 8]), 7)
-
-
-if __name__ == '__main__':
-    unittest.main()
-
-
-# Doctest-based testing
-def median(pool):
-    '''Statistical median to demonstrate doctest.
-    >>> median([2, 9, 9, 7, 9, 2, 4, 5, 8])
-    6 #change to 7 in order to pass the test
-    '''
-    copy = sorted(pool)
-    size = len(copy)
-    if size % 2 == 1:
-        return copy[int((size - 1) / 2)]
-    else:
-        return (copy[int(size / 2 - 1)] + copy[int(size / 2)]) / 2
-
-
-if __name__ == '__main__':
-    import doctest
-
-    doctest.testmod()
 # 8-Queens Problem (recursion)
 BOARD_SIZE = 8
 
@@ -135,11 +78,69 @@ def solve(n):
     if n == 0:
         return [[]]
     smaller_solutions = solve(n - 1)
-    return [solution + [(n, i + 1)]
-            for i in range(BOARD_SIZE)
+    return [solution + [(n, a + 1)]
+            for a in range(BOARD_SIZE)
             for solution in smaller_solutions
-            if not under_attack(i + 1, solution)]
+            if not under_attack(a + 1, solution)]
 
 
 for answer in solve(BOARD_SIZE):
     print(answer)
+
+# itertools
+# noqa from itertools import groupby
+lines = '''
+This is the
+first paragraph.
+
+This is the second.
+'''.splitlines()
+# Use itertools.groupby and bool to return groups of
+# consecutive lines that either have content or don't.
+for has_chars, frags in groupby(lines, bool):
+    if has_chars:
+        print(' '.join(frags))
+
+
+# PRINTS:
+# This is the first paragraph.
+# This is the second.
+
+# Doctest-based testing
+def median(pool):
+    """Statistical median to demonstrate doctest.
+    >>> median([2, 9, 9, 7, 9, 2, 4, 5, 8])
+    6 #change to 7 in order to pass the test
+    """
+    copy = sorted(pool)
+    size = len(copy)
+    if size % 2 == 1:
+        return copy[int((size - 1) / 2)]
+    else:
+        return (copy[int(size / 2 - 1)] + copy[int(size / 2)]) / 2
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
+
+
+# Unit testing with unittest
+# noqa import unittest
+def median(pool):
+    copy = sorted(pool)
+    size = len(copy)
+    if size % 2 == 1:
+        return copy[int((size - 1) / 2)]
+    else:
+        return (copy[int(size / 2 - 1)] + copy[int(size / 2)]) / 2
+
+
+class TestMedian(unittest.TestCase):
+    def testMedian(self):
+        self.assertEqual(median([2, 9, 9, 7, 9, 2, 4, 5, 8]), 7)
+
+
+if __name__ == '__main__':
+    unittest.main()
