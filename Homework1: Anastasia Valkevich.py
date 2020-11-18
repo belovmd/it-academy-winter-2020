498fe4034f0c0a7e984b960dd1ca690e84d9eb3d
sailorbl <72577047+sailorbl@users.noreply.github.com>
2020-11-18 12:36:53 +0200

    Homework1_fix2

--- Homework1: Anastasia Valkevich.py
+++ Homework1: Anastasia Valkevich.py
@@ -14,25 +14,22 @@ import random
 print('Hello, world!')
 
 # 2 lines: Input, assignment
-
 name = input('What is your name?\n')
 print('Hi, %s.' % name)
 
 # 3 lines: For loop, built-in enumerate function, new style formatting
-
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
     print("iteration {iteration} is {name}".format(iteration=i, name=name))
 
-# 4 lines: Fibonacci, tuple assignment
+    # 4 lines: Fibonacci, tuple assignment
 
 parents, babies = (1, 1)
 while babies < 100:
     print('This generation has {0} babies'.format(babies))
     parents, babies = (babies, parents + babies)
 
-
-# 5 lines: Functions
+    # 5 lines: Functions
 
 
 def greet(name):
@@ -53,7 +50,6 @@ for test_string in ['555-1212', 'ILL-EGAL']:
 
 # 7 lines: Dictionaries, generator expressions
 
-
 prices = {'apple': 0.40, 'banana': 0.50}
 my_purchase = {
     'apple': 1,
@@ -64,8 +60,8 @@ print('I owe the grocer $%.2f' % grocery_bill)
 
 # 8 lines: Command line arguments, exception handling
 
-# This program adds up integers that have been passed \
-# as arguments in the command line
+# This program adds up integers that have been passed as arguments in the command line
+
 try:
     total = sum(int(arg) for arg in sys.argv[1:])
     print('sum =', total)
@@ -89,7 +85,6 @@ for file_name in sorted(python_files):
 
 # 10 lines: Time, conditionals, from..import, for..else
 
-
 activities = {8: 'Sleeping',
               9: 'Commuting',
               17: 'Working',
@@ -109,7 +104,6 @@ else:
 
 # 11 lines: Triple-quoted strings, while loop
 
-
 REFRAIN = '''
  %d bottles of beer on the wall,
  %d bottles of beer,
@@ -122,8 +116,7 @@ while bottles_of_beer > 1:
                      bottles_of_beer - 1))
     bottles_of_beer -= 1
 
-
-# 12 lines: Classes
+    # 12 lines: Classes
 
 
 class BankAccount(object):
@@ -147,7 +140,6 @@ print(my_account.balance, my_account.overdrawn())
 
 # 13 lines: Unit testing with unittest
 
-
 def median(pool):
     copy = sorted(pool)
     size = len(copy)
@@ -167,7 +159,6 @@ if __name__ == '__main__':
 
 
 # 14 lines: Doctest-based testing
-
 
 def median(pool):
     """Statistical median to demonstrate doctest.
@@ -189,14 +180,13 @@ if __name__ == '__main__':
 
 # 15 lines: itertools
 
-
 lines = '''
  This is the
  first paragraph.
 
- This is the second.
+  This is the second.
  '''.splitlines()
-# Use itertools.groupby and bool to return groups of
+# Use itertools.group by and bool to return groups of
 # consecutive lines that either have content or don't.
 for has_chars, frags in groupby(lines, bool):
     if has_chars:
@@ -206,7 +196,6 @@ for has_chars, frags in groupby(lines, bool):
 # PRINTS:
 # This is the first paragraph.
 # This is the second.
-
 
 # 16 lines: csv module, tuple unpacking, cmp() built-in
 
@@ -229,9 +218,9 @@ with open('stocks.csv', 'r') as stocksFile:
     stocks = csv.reader(stocksFile)
 
     status_labels = {-1: 'down', 0: 'unchanged', 1: 'up'}
-    for ticker, name, price, change, pct in stocks:
-        status = status_labels[cmp(float(change), 0.0)]
-        print('%s is %s (%.2f)' % (name, status, float(pct)))
+for ticker, name, price, change, pct in stocks:
+    status = status_labels[cmp(float(change), 0.0)]
+    print('%s is %s (%.2f)' % (name, status, float(pct)))
 
 # 18 lines: 8-Queens Problem (recursion)
 
@@ -246,7 +235,7 @@ def under_attack(col, queens):
 
         if c in (left, col, right):
             return True
-    return False
+        return False
 
 
 def solve(n):
@@ -267,7 +256,6 @@ for answer in solve(BOARD_SIZE):
 
 # 20 lines: Prime numbers sieve w/fancy generators
 
-
 def iter_primes():
     # an iterator of all numbers between 2 and +infinity
     numbers = itertools.count(2)
@@ -287,18 +275,18 @@ for p in iter_primes():
     if p > 1000:
         break
     print(p)
+
 # 21 lines: XML/HTML parsing
 
 dinner_recipe = '''<html><body><table>
-<tr><th>amt</th><th>unit</th><th>item</th></tr>
-<tr><td>24</td><td>slices</td><td>baguette</td></tr>
-<tr><td>2+</td><td>tbsp</td><td>olive oil</td></tr>
-<tr><td>1</td><td>cup</td><td>tomatoes</td></tr>
-<tr><td>1</td><td>jar</td><td>pesto</td></tr>
-</table></body></html>'''
+ <tr><th>amt</th><th>unit</th><th>item</th></tr>
+ <tr><td>24</td><td>slices</td><td>baguette</td></tr>
+ <tr><td>2+</td><td>tbsp</td><td>olive oil</td></tr>
+ <tr><td>1</td><td>cup</td><td>tomatoes</td></tr>
+ <tr><td>1</td><td>jar</td><td>pesto</td></tr>
+ </table></body></html>'''
 
 # From http://effbot.org/zone/element-index.htm
-
 
 tree = etree.fromstring(dinner_recipe)
 
@@ -306,13 +294,13 @@ tree = etree.fromstring(dinner_recipe)
 # import ElementSoup, StringIO
 # tree = ElementSoup.parse(StringIO.StringIO(dinner_recipe))
 
-pantry = set(['olive oil', 'pesto'])
+pantry = {'olive oil', 'pesto'}
 for ingredient in tree.getiterator('tr'):
     amt, unit, item = ingredient
     if item.tag == "td" and item.text not in pantry:
         print("%s: %s %s" % (item.text, amt.text, unit.text))
 
-# 28 lines: 8-Queens Problem (define your own exceptions)
+    # 28 lines: 8-Queens Problem (define your own exceptions)
 
 BOARD_SIZE = 8
 
@@ -347,8 +335,7 @@ queens = add_queen([])
 print(queens)
 print("\n".join(". " * q + "Q " + ". " * (BOARD_SIZE - q - 1) for q in queens))
 
-# 33 lines: "Guess the Number" Game (edited) from
-
+# 33 lines: "Guess the Number" Game (edited) from http://inventwithpython.com
 
 guesses_made = 0
 
@@ -358,16 +345,21 @@ number = random.randint(1, 20)
 print('Well, {0}, I am thinking of a number between 1 and 20.'.format(name))
 
 while guesses_made < 6:
+
     guess = int(input('Take a guess: '))
+
     guesses_made += 1
+
     if guess < number:
-        print('Your guess is too low.')
+        print('Your guess is too low')
+
     if guess > number:
-        print('Your guess is too high.')
+        print('Your guess is too high')
+
     if guess == number:
         break
+
 if guess == number:
-    print('Good job, {0}! You guessed my number in {1} guesses!' \
-          .format(name, guesses_made))
+    print('Good job, {0}! You guessed my number in {1} guesses!'.format(name, guesses_made))
 else:
     print('Nope. The number I was thinking of was {0}'.format(number))
