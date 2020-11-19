102df36045af4523ccc109d2c07f2c0078b2d3f7
Anton <gorulev2020@inbox.ru>
2020-11-19 19:28:09 +0300

    second_

--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,8 @@ import glob
 import unittest
 import csv
 import itertools
+import xml.etree.ElementTree as etree
+import random
 
 from time import localtime
 from itertools import groupby
@@ -269,7 +271,6 @@ dinner_recipe = '''<html><body><table>
 </table></body></html>'''
 
 # From http://effbot.org/zone/element-index.htm
-import xml.etree.ElementTree as etree
 tree = etree.fromstring(dinner_recipe)
 
 # For invalid HTML use http://effbot.org/zone/element-soup.htm
@@ -280,4 +281,66 @@ pantry = set(['olive oil', 'pesto'])
 for ingredient in tree.getiterator('tr'):
     amt, unit, item = ingredient
     if item.tag == "td" and item.text not in pantry:
-        print ("%s: %s %s" % (item.text, amt.text, unit.text))
+        print("%s: %s %s" % (item.text, amt.text, unit.text))
+
+# 28 lines: 8-Queens Problem (define your own exceptions)
+BOARD_SIZE = 8
+
+
+class BailOut(Exception):
+    pass
+
+
+def validate(queens):
+    left = right = col = queens[-1]
+    for r in reversed(queens[:-1]):
+        left, right = left - 1, right + 1
+        if r in (left, col, right):
+            raise BailOut
+
+
+def add_queen(queens):
+    for i in range(BOARD_SIZE):
+        test_queens = queens + [i]
+        try:
+            validate(test_queens)
+            if len(test_queens) == BOARD_SIZE:
+                return test_queens
+            else:
+                return add_queen(test_queens)
+        except BailOut:
+            pass
+    raise BailOut
+
+
+queens = add_queen([])
+print(queens)
+print("\n".join(". " * q + "Q " + ". " * (BOARD_SIZE - q - 1) for q in queens))
+
+# 33 lines: "Guess the Number" Game (edited) from http://inventwithpython.com
+guesses_made = 0
+
+name = input('Hello! What is your name?\n')
+
+number = random.randint(1, 20)
+print('Well, {0}, I am thinking of a number between 1 and 20.'.format(name))
+
+while guesses_made < 6:
+
+    guess = int(input('Take a guess: '))
+
+    guesses_made += 1
+
+    if guess < number:
+        print('Your guess is too low.')
+
+    if guess > number:
+        print('Your guess is too high.')
+
+    if guess == number:
+        break
+
+if guess == number:
+    print('Good job, {0}! You guessed my number in {1} guesses!'.format(name, guesses_made))
+else:
+    print('Nope. The number I was thinking of was {0}'.format(number))
