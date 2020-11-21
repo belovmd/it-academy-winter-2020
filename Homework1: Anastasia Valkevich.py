91a38671dd822e836be21d881c24f8b1e38c2647
sailorbl <72577047+sailorbl@users.noreply.github.com>
2020-11-21 22:41:53 +0200

    Homework1_fix6

--- Homework1: Anastasia Valkevich.py
+++ Homework1: Anastasia Valkevich.py
@@ -1,18 +1,13 @@
-import glob
 import csv
+import glob
 import itertools
 from itertools import groupby
 import random
-
 import re
 import sys
 from time import localtime
 import unittest
 import xml.etree.ElementTree as etree
-
-
-
-
 
 # 1 line: Output
 
@@ -175,7 +170,6 @@ if __name__ == '__main__':
 
 
 def median(pool):
-
     """Statistical median to demonstrate doctest.
 
     >>> median([2, 9, 9, 7, 9, 2, 4, 5, 8])
@@ -379,7 +373,7 @@ while guesses_made < 6:
         break
 
 if guess == number:
-    print('Good job, {0}! You guessed my number in {1} guesses!'\
+    print('Good job, {0}! You guessed my number in {1} guesses!' \
           .format(name, guesses_made))
 else:
     print('Nope. The number I was thinking of was {0}'.format(number))
