c3bef0e80b50db6be61f3a208dcfaa0313da422f
MaximShamiakov <shamiakov@bk.ru>
2020-11-21 15:33:39 +0300

    First commit test

--- src/__init__.py
+++ src/__init__.py
@@ -168,8 +168,7 @@ def median1(pool):
 
 if name == '__main__':
     import doctest
-    doctest.te
-stmod()
+    doctest.testmod()
 
 # 15 lines: itertools
 lines = '''
@@ -327,8 +326,7 @@ guesses_made = 0
 
 name = input('Hello! What is your name?\n')
 
-number = rand
-om.randint(1, 20)
+number = random.randint(1, 20)
 print('Well, {0}, I am thinking of a number between 1 and 20.'.format(name))
 
 while guesses_made < 6:
