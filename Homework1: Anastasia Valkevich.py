98cd28b5cd6948b9769ecae788f6f4cff2d2413b
sailorbl <72577047+sailorbl@users.noreply.github.com>
2020-11-28 22:13:53 +0200

    Homework1_fix7 with double lines

--- Homework1: Anastasia Valkevich.py
+++ Homework1: Anastasia Valkevich.py
@@ -10,17 +10,21 @@ import xml.etree.cElementTree as groupby
 from itertools import groupby
 from time import localtime
 
+
 # 1 line: Output
 print('Hello, world!')
+
 
 # 2 lines: Input, assignment
 name = input('What is your name?\n')
 print('Hi, %s.' % name)
 
+
 # 3 lines: For loop, built-in enumerate function, new style formatting
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
     print("iteration {iteration} is {name}".format(iteration=i, name=name))
+
 
 # 4 lines: Fibonacci, tuple assignment
 parents, babies = (1, 1)
@@ -38,12 +42,14 @@ greet('Jack')
 greet('Jill')
 greet('Bob')
 
+
 # 6 lines: Import, regular expressions
 for test_string in ['555-1212', 'ILL-EGAL']:
     if re.match(r'^\d{3}-\d{4}$', test_string):
         print(test_string, 'is a valid US local phone number')
     else:
         print(test_string, 'rejected')
+
 
 # 7 lines: Dictionaries, generator expressions
 prices = {'apple': 0.40, 'banana': 0.50}
@@ -54,6 +60,7 @@ grocery_bill = sum(prices[fruit] * my_purchase[fruit]
                    for fruit in my_purchase)
 print('I owe the grocer $%.2f' % grocery_bill)
 
+
 # 8 lines: Command line arguments, exception handling
 # This program adds up integers that have been passed \
 # as arguments in the command line
@@ -62,6 +69,7 @@ try:
     print('sum =', total)
 except ValueError:
     print('Please supply integer arguments')
+
 
 # 9 lines: Opening files
 
@@ -76,6 +84,7 @@ for file_name in sorted(python_files):
             print('    ' + line.rstrip())
 
     print()
+
 
 # 10 lines: Time, conditionals, from..import, for..else
 activities = {8: 'Sleeping',
@@ -94,6 +103,7 @@ for activity_time in sorted(activities.keys()):
         break
 else:
     print('Unknown, AFK or sleeping!')
+
 
 # 11 lines: Triple-quoted strings, while loop
 REFRAIN = '''
