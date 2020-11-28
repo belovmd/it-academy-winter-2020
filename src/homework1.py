769d692593802e1d83173ff3b736dac7179d576b
yanix <smirnovskoe@gmail.com>
2020-11-28 13:55:36 +0300

    fix blank lines

--- src/homework1.py
+++ src/homework1.py
@@ -3,30 +3,28 @@ import random
 import re
 
 # 1 line: Output
-
 print('Hello, world!')
 
-# 2 lines: Input, assignment
 
+# 2 lines: Input, assignment
 name = input('What is your name?\n')
 print('Hi, %s.' % name)
 
-# 3 lines: For loop, built-in enumerate function, new style formatting
 
+# 3 lines: For loop, built-in enumerate function, new style formatting
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
     print("iteration {iteration} is {name}".format(iteration=i, name=name))
 
-# 4 lines: Fibonacci, tuple assignment
 
+# 4 lines: Fibonacci, tuple assignment
 parents, babies = (1, 1)
 while babies < 100:
     print('This generation has {0} babies'.format(babies))
     parents, babies = (babies, parents + babies)
 
+
 # 5 lines: Functions
-
-
 def greet(name):
     print('Hello', name)
 
@@ -35,17 +33,16 @@ greet('Jack')
 greet('Jill')
 greet('Bob')
 
-# 6 lines: Import, regular expressions
 
+# 6 lines: Import, regular expressions
 for test_string in ['555-1212', 'ILL-EGAL']:
     if re.match(r'^\d{3}-\d{4}$', test_string):
         print(test_string, 'is a valid US local phone number')
     else:
         print(test_string, 'rejected')
 
+
 # 20 lines: Prime numbers sieve w/fancy generators
-
-
 def iter_primes():
     '''an iterator of all numbers between 2 and +infinity'''
     numbers = itertools.count(2)
@@ -66,8 +63,8 @@ for p in iter_primes():
         break
     print(p)
 
-# 28 lines: 8-Queens Problem (define your own exceptions)
 
+# 28 lines: 8-Queens Problem (define your own exceptions)
 BOARD_SIZE = 8
 
 
@@ -101,8 +98,8 @@ queens = add_queen([])
 print(queens)
 print("\n".join(". " * q + "Q " + ". " * (BOARD_SIZE - q - 1) for q in queens))
 
-# 33 lines: "Guess the Number" Game (edited) from http://inventwithpython.com
 
+# 33 lines: "Guess the Number" Game (edited) from http://inventwithpython.com
 guesses_made = 0
 
 name = input('Hello! What is your name?\n')
