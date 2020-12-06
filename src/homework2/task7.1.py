f1716aa0b1c0b503222b6b311cf94aa3bec880af
yanix <smirnovskoe@gmail.com>
2020-12-06 12:48:41 +0300

    Fix long lines

--- src/homework2/task7.1.py
+++ src/homework2/task7.1.py
@@ -3,12 +3,15 @@ Task name: No Idea!
 Source: HackerRank
 Level: Medium
 
-There is an array of "n" integers. There are also 2 disjoint sets, "A" and "B", each containing  integers.
-You like all the integers in set A and dislike all the integers in set B. Your initial happiness is 0.
-For each "i" integer in the array, if "i in A", you add 1 to your happiness. If "i in B", you add -1 to your happiness.
-Otherwise, your happiness does not change. Output your final happiness at the end.
+There is an array of "n" integers. There are also 2 disjoint sets, "A"
+and "B", each containing  integers.You like all the integers in set A and
+dislike all the integers in set B. Your initial happiness is 0.
+For each "i" integer in the array, if "i in A", you add 1 to your happiness.
+If "i in B", you add -1 to your happiness. Otherwise, your happiness does
+not change. Output your final happiness at the end.
 
-Note: Since "A" and "B" are sets, they have no repeated elements. However, the array might contain duplicate elements.
+Note: Since "A" and "B" are sets, they have no repeated elements.
+However, the array might contain duplicate elements.
 """
 
 
@@ -44,10 +47,3 @@ if __name__ == "__main__":
         print('happiness is ', get_happiness(set_b, set_a, input_list))
     else:
         print("Множества А и В имеют пересечения (общие элементы).")
-
-
-
-
-
-
-
