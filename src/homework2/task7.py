97db2cbb0f6caffa6b316a7c19076505d5417833
anitsirKOs <72756360+anitsirKOs@users.noreply.github.com>
2020-12-12 21:27:04 +0300

    Hw_2: fix bugs and add 2 tasks, pass tests

--- src/homework2/task7.py
+++ src/homework2/task7.py
@@ -76,7 +76,7 @@ print(is_leap())
 
 """5. Word Order
     Difficulty Medium
- 
+
     You are given  words. Some words may repeat.
     For each word, output its number of occurrences.
     The output order should correspond with the input order
