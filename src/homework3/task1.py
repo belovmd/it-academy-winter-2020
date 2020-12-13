49f837371c788636716b640ee24f333d3c3bc9ff
yanix <smirnovskoe@gmail.com>
2020-12-13 17:15:56 +0300

    Add tasks: 1, 3, 6

--- src/homework3/task1.py
+++ src/homework3/task1.py
@@ -1,6 +1,25 @@
+""" Task 1: FizzBuzz
+Напишите программу, которая печатает цифры от 1 до 100, но вместо чисел,
+кратных 3 пишет Fizz, вместо чисел кратный 5 пишет Buzz, а вместо чисел
+одновременно кратных и 3 и 5 - FizzBuzz
+"""
+
 
 def fizzbuzz():
-    pass
+    """
+
+    :return: None
+    """
+    for number in range(1, 101):
+        if (not number % 3) and (not number % 5):
+            print('FizzBuzz')
+            continue
+        if not number % 3:
+            print('Fizz')
+            continue
+        if not number % 5:
+            print('Buzz')
+            continue
 
 
 if __name__ == '__main__':
