2a9902f5d3ae4d3551d7536e3b7b7caafb611476
Belaid86 <belaid.ruman@gmail.com>
2020-12-14 21:17:44 +0300

    Homework3: Belaid Ruman

---
+++ src/task1.py
@@ -0,0 +1,15 @@
+"""Напишите программу, которая печатает цифры от 1 до 100,
+но вместо чисел, кратных 3 пишет Fizz, вместо чисел кратный
+5 пишет Buzz, а вместо чисел одновременно кратных и 3 и 5 - FizzBuzz
+"""
+
+
+for element in range(1, 101):
+    if element % 3 == 0 and element % 5 == 0:
+        print('FIZZBUZZ')
+    elif element % 5 == 0:
+        print('BUZZ')
+    elif element % 3 == 0:
+        print('FIZZ')
+    else:
+        print(element)
