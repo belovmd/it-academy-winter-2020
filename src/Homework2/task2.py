e6c5bb63ca090231be491790dbcf0316043ff29e
MaximShamiakov <shamiakov@bk.ru>
2020-12-17 18:55:30 +0300

    changes

--- src/Homework2/task2.py
+++ src/Homework2/task2.py
@@ -2,29 +2,11 @@
 Найти самое длинное слово в введенном предложении.
 Учтите что в предложении есть знаки препинания.
 """
+import re
+
 sentence = input('введите предложение: ')
-
-
-def longest_word(sentence, punctuation=',.?!:;"()-'):
-    for symbol in punctuation:
-        sentence = sentence.replace(symbol, '')
-        # убираем из предложения символы пунктуации
-    words = sentence.split(' ')
-    # разделяем предложение на слова
-    longest_word = ''
-
-    for word in words:
-        if not word.isalpha():
-            # проверяет, состоит ли слово из букв, числа не учитываем.
-            continue
-
-        if len(word) > len(longest_word):
-            # если текущее слово больше последнего самого длинного,
-            longest_word = word
-            # то заменыем самое длинное текущим словом
-
-    return longest_word
-
-
+def longest_word(sentence):
+    words = re.sub('[^\w ]', '', sentence).split(' ')
+    return max(words, key=len)
 longest = longest_word(sentence)
-print(f'Самое длинное слово - "{longest}". Длинна {len(longest)} символов.')
+print(f'Самое длинное слово - "{longest}". {len(longest)} символов.')
