5b5fb8cee8fd933bb9640c0966b73bd113b68f82
dambo-dambo <denis_borisenko@mail.ru>
2020-12-19 18:41:28 +0300

    solve task, add advanced tasks

--- src/homework2/task2.py
+++ src/homework2/task2.py
@@ -1,7 +1,13 @@
+import string
+
 """Найти самое длинное слово в введенном предложении.
    Учтите что в предложении есть знаки препинания.
 """
 
-string = input('Введите предложение: ')
-long_word = max(string.split(), key=len)
+in_str = input('Введите предложение: ')
+element = string.punctuation
+for i in range(len(element)):
+    in_str = in_str.replace(element[i], "")
+
+long_word = max(in_str.split(), key=len)
 print(f'Самое длинное слово в предложении:{long_word}')
