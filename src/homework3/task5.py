3f1a9921d12c49a5b50bf7cd7491554d77fdf33f
v.kandrashova <vict8ria.k@gmail.com>
2020-12-27 00:41:02 +0300

    fix for test

--- src/homework3/task5.py
+++ src/homework3/task5.py
@@ -6,6 +6,6 @@
     в списке.
 """
 
-list_of_numbers = [1, [7], 10, 4, "str", 8, 10, "one more str", 32, 12, "str", [7]]
-unique_elements = [el for el in list_of_numbers if list_of_numbers.count(el) == 1]
+num_list = [1, [7], 10, 4, "str", 8, 10, "one more str", 32, 12, "str", [7]]
+unique_elements = [el for el in num_list if num_list.count(el) == 1]
 print(*unique_elements, sep=", ")
