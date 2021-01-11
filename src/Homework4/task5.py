4e3150fb0a9803770c2b3840818a7b59ca435cd3
Belaid86 <belaid.ruman@gmail.com>
2021-01-11 12:25:40 +0300

    Homework4 - corrections 4

--- src/Homework4/task5.py
+++ src/Homework4/task5.py
@@ -32,9 +32,12 @@ language_num_3rd = 3
 languages_3rd = ['Russian', 'French', 'Italian']
 Quantity_of_languages = len(set(languages_1st + languages_2nd + languages_3rd))
 print("Все школьники знают", Quantity_of_languages, "языков)")
-List_of_languages = set(languages_1st) & set(languages_2nd) & set(languages_3rd)
+List_of_languages = set(languages_1st) & set(languages_2nd) \
+                & set(languages_3rd)
 print("Список языков, которые знают все школьники", List_of_languages)
-at_list_1_pupil = len(set(languages_3rd) - set(languages_1st) - set(languages_2nd))
+at_list_1_pupil = len(set(languages_3rd) - set(languages_1st)
+                      - set(languages_2nd))
 print("Кол-во языков, которые знает хотя бы один школьник", at_list_1_pupil)
-list_at_list_1_pipil = set(languages_3rd) - set(languages_1st) - set(languages_2nd)
+list_at_list_1_pipil = set(languages_3rd) - set(languages_1st) \
+                       - set(languages_2nd)
 print("Например: ", list_at_list_1_pipil)
