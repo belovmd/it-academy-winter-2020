552a62b16e2d3bb6ca66ba82767311fcc09806ae
Evgeniy Asipkov <easipkov@gmail.com>
2021-01-12 16:42:51 +0300

    homework3 fix task4 and task5 0(n)

--- src/homework3/task4.py
+++ src/homework3/task4.py
@@ -6,11 +6,19 @@
 # Выходные данные - количество пар.
 # Важно: 1 1 1 - это 3 пары, 1 1 1 1 - это 6 пар
 str_num = '2 2 2 2 4 5 6 6 6 6'
-str_num = str_num.replace(' ', '')
+dct_str_num = {}
+result_num = []
 num_pairs = 0
-for num in str_num:
-    # формула подсчета пар
-    # Кол-во пар = (всего элементов X всего элементов — 1) / 2
-    num_pairs += (str_num.count(num) * (str_num.count(num) - 1)) / 2
-    str_num = str_num.replace(num, '')
-print(int(num_pairs))
+
+for element in str_num.split():
+    dct_str_num[element] = dct_str_num.get(element, 0) + 1
+
+for val_ in dct_str_num.values():
+    if val_ > 1:
+        # формула подсчета пар
+        # Кол-во пар = (всего элементов X всего элементов — 1) / 2
+        result_num.append(val_ * ((val_ - 1) / 2))
+
+for num in result_num:
+    num_pairs += num
+print(num_pairs)
--- src/homework3/task5.py
+++ src/homework3/task5.py
@@ -3,10 +3,11 @@
 # только один раз. Элементы нужно выводить в том порядке, в котором
 # они встречаются в списке.
 list_all = [2, 4, 2, 'a', 5, 'a', 7, 6, 6, 6]
+dct_list_all = {}
 result = ''
 for el in list_all:
-    if list_all.count(el) == 1:
-        result += str(el)
-while list_all.count(el) > 1:
-    list_all.remove(el)
-print(result)
+    dct_list_all[el] = dct_list_all.get(el, 0) + 1
+
+for key, value in dct_list_all.items():
+    if value == 1:
+        print(key)
