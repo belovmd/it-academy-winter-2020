4f793879c9e43daa605fff8ca34c99da33748cb9
anitsirKOs <72756360+anitsirKOs@users.noreply.github.com>
2021-01-14 23:24:22 +0300

    homework4

---
+++ src/homework4/task6.py
@@ -0,0 +1,12 @@
+"""Слова
+    Во входной строке записан текст. Словом считается последовательность
+    непробельных символов идущих подряд, слова разделены одним или большим
+    числом пробелов или символами конца строки. Определите, сколько различных
+    слов содержится в этом тексте.
+"""
+
+
+my_string = ('fcgh gvf g gfgfg. hgfu.   kjhgf/kjbh.kdjhabcs.dfj335gdkhgj.srgh\n'
+             'jhgfg?nbhvdfg gfdtyug   hgfu. hgfu. hgfu. hgfu.    hjfdgfghjb')
+
+print(len(set(my_string.split())), my_string.split())
